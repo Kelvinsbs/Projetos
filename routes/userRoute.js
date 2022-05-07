0743c7640ead5d374ec4b7dd9217bc842aa4fe15
Kelvinsbs <nivlek.barcelos@gmail.com>
2022-05-07 20:06:38 -0300

    rotas basicas criadas e funcionando

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,4 +1,5 @@
 const { json } = require('body-parser');
+const res = require('express/lib/response');
 const fs = require('fs');
 const { join } = require('path')
 
@@ -6,9 +7,9 @@ const filePath = join(__dirname, 'user.json');
 
 const getUsers = () => {
     const data = fs.existsSync(filePath) ? fs.readFileSync(filePath) : [];
-
+    
     try {
-        json.parse(data)
+        return JSON.parse(data)
     } catch (error) {
         return [];
     }
@@ -19,9 +20,40 @@ const saveUsers = (users) => fs.writeFileSync(filePath, JSON.stringify(users, nu
 const userRoute = (app) => {
     app.route('/users/:id?')
         .get((req, res) => {
-            const user = getUsers();
+            const users = getUsers();
 
-            res.send({ user })
+            res.send({ users })
+        })
+        .post((req, res) => {
+            const users = getUsers();
+
+            users.push(req.body)
+            saveUsers(users)
+
+            res.status(201).send('OK')
+        })
+        .put((req, res) => {
+            const users = getUsers();
+
+            saveUsers(users.map(user => {
+                if(user.id === req.params.id){
+                    return {
+                        ...user,
+                        ...req.body
+                    }
+                }
+
+                return user;
+            }))
+
+            res.status(200).send('OK')
+        })
+        .delete((req, res) => {
+            const users = getUsers();
+
+            saveUsers(users.filter(user => user.id !== req.params.id))
+
+            res.status(200).send('OK')
         })
 }
 
